c63e93b825709338007565a22197fe30ff0a6c05
Takuya Kikuchi <kikuchi.takuya.dbce8@gmail.com>
2020-02-16 18:16:50 +0900

    added delete function

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'cocktails#index'
-  resources :cocktails, except: [:edit, :update, :destroy] do
+  resources :cocktails, except: [:edit, :update] do
     resources :doses, only: [:new, :create]
   end
   resources :doses, only: :destroy
